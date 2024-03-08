c0aac8a0ee34c0633ac0166e8169d34306101c81
Natdanai <frame.sk140@gmail.com>
2024-03-08 19:36:12 +0700

    new branch

--- Touchgrass/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Touchgrass.Models
-{
-    public class User
-    {
-        public string? Name { get; set; }
-        public int Ids { get; set; }
-        public string? Pic { get; set; }
-    }
-}
---
+++ wwwroot/js/login_signup.js
@@ -0,0 +1,45 @@
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide"),
+  appleIdBtn = document.querySelector("#oa2-apple-id"),
+  facebookBtn = document.querySelector("#oa2-facebook"),
+  googleBtn = document.querySelector("#oa2-google-oauth2"),
+  microsoftBtn = document.querySelector("#oa2-azuread-oauth2"),
+  spanText =
+    document.querySelector(".sr-only"); /*sign in/up with apple blah blah*/
+
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+/*test*/
+appleIdBtn.addEventListener("click", function () {
+  spanText.classList.toggle("show");
+});
