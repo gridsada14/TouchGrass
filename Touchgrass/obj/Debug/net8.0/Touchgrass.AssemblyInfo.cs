74f2ac3e08d0fd933bb5c3a28c06143665495232
Oum789 <65010027@kmitl.ac.th>
2024-03-11 00:54:46 +0700

    change color of setting button

--- Touchgrass/obj/Debug/net8.0/Touchgrass.AssemblyInfo.cs
+++ Touchgrass/obj/Debug/net8.0/Touchgrass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Touchgrass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71dd2093bb0cd04c477d422bdec593d9378a374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782075aacaa9231067b4c17c8b33cc6c2557a55b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Touchgrass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Touchgrass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
